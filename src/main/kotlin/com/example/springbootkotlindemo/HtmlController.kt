7fdd1b3b9174c1d39bbf443c5045b7f96e82c7f0
Ashish Sharda <ashishjsharda@gmail.com>
2023-01-07 18:46:25 -0600

    add missing dependencies

--- src/main/kotlin/com/example/springbootkotlindemo/HtmlController.kt
+++ src/main/kotlin/com/example/springbootkotlindemo/HtmlController.kt
@@ -9,12 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping
 class HtmlController {
     @GetMapping("/")
     fun blog(model: Model): String {
-        model.addAttribute("title", "Blog")
-        model.addAttribute("posts", listOf(
-            Post("Spring 5.0 goes GA", "Dear Spring community ..."),
-            Post("Reactor Bismuth is out", "Lorem ipsum ..."),
-            Post("Spring Data Kay-RC1", "Lorem ipsum ...")
-        ))
+        model["title"] = "Blog"
         return "blog"
     }
 }
